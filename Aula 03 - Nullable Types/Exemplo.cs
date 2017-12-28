d3297746b4c149ddfe731408b3274137e6e2413e
Felip3FL <felip3.fl@gmail.com>
2017-12-28 14:00:36 -0200

    Add project files.

---
+++ Aula 03 - Nullable Types/Exemplo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Aula_03___Nullable_Types
+{
+    class Exemplo
+    {
+        private void ExemploDaVariaveis()
+        {
+
+            //Essa linha somes com as msg de warning do C#
+            #pragma warning disable 0, CS0219
+
+            string nome = null;
+
+            //Essa linha dá erro, porque int não pode receber valor em branco ou nulo
+            //int valor1 = null;
+
+            int? valor1 = null;
+            Nullable<int> valor2 = null;
+
+            double? d = 3.14;
+            bool? flag = null;
+            char? letra = 'A';
+            int?[] array = new int?[10];
+
+        }
+    }
+}
---
+++ Aula 03 - Nullable Types/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Aula_03___Nullable_Types
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            /////////////////////////////////////////////////////////////////////////////////
+
+            //Atribuindo valor e exibindo na tela
+            int? valor1 = null;
+            Console.WriteLine("Valor 1:");
+            Console.WriteLine("{0} ", valor1.ToString());
+
+            //Atribuindo valor e exibindo na tela
+            int? valor2 = 90;
+            Console.WriteLine("Valor 2:");
+            Console.WriteLine("{0} ", valor2);
+
+            //Conversão
+            int? valor3 = 100;              //implicitamente
+            int valor4 = (int)valor3;       //explicitamente usando CAST -> (int)
+
+            /////////////////////////////////////////////////////////////////////////////////
+
+
+          //DateTime? dataNascimento = DateTime.now;
+            DateTime? dataNascimento = null;
+            if (dataNascimento.HasValue)
+            {
+                Console.WriteLine("Data de nascimento foi definida");
+            }
+            else
+            {
+                Console.WriteLine("Data de nascimento é null");
+            }
+
+            //A propriedade Value retorna um valor se houver um atribuído, 
+            //de outra forma será lançada uma exceção System.InvalidOperationException;
+            try
+            {
+                Console.WriteLine(dataNascimento.Value);
+            } catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+                Console.ReadKey();
+            }
+            
+
+
+            /////////////////////////////////////////////////////////////////////////////////
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}

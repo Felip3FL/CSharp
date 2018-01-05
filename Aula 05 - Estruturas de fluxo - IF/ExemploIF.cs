d47eec922a9f03fa03fa8e19a569670ab26dd4f2
Felip3FL <felip3.fl@gmail.com>
2018-01-05 13:24:49 -0200

    Aula 05 - Estruturas de fluxo

---
+++ Aula 05 - Estruturas de fluxo - IF/ExemploIF.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_05___Estruturas_de_fluxo___IF
+{
+    class ExemploIF
+    {
+        static void Main(string[] args)
+        {
+
+            bool a = true, b = false;
+            if (a) if (b) System.Console.WriteLine("a e b são true"); else System.Console.WriteLine("a é true e b é false");
+            else System.Console.WriteLine("a é false");
+
+
+            if (a)
+            {
+                if (b)
+                {
+                    System.Console.WriteLine("a e b são true");
+                }
+                else
+                {
+                    System.Console.WriteLine("a é true e b é false");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("a é false");
+            }
+
+            System.Console.ReadKey();
+
+        }
+    }
+}
---
+++ Aula 05 - Estruturas de fluxo - IF/ExemploSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_05___Estruturas_de_fluxo___IF
+{
+    class ExemploSwitch
+    {
+        public void Exemplo()
+        {
+
+            int numero = 1;
+            switch (numero)
+            {
+                case 0:
+                    Console.WriteLine("O número é zero");
+                    break;
+                case 2 - 1:
+                    Console.WriteLine("O número é 1!");
+                    break;
+            }
+
+            System.Console.ReadKey();
+
+        }
+    }
+}

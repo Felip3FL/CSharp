0dfcd334dea61cc11863e37feceab7fada1d2bc6
Felipe Lima <felip3.fl@msn.com>
2018-07-06 20:39:44 -0300

    Aula 14 - Metodos

---
+++ Aula 14 - Metodos/PassagemDeParametrosPorValorReferenciaOUT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_14___Metodos
+{
+    class PassagemDeParametrosPorValorReferenciaOUT
+    {
+
+        static void CalculaValor_PorValor(int valor)
+        {
+            valor = valor + 1;
+        }
+
+        public void PorValor()
+        {
+            int numero = 500;
+
+            Console.WriteLine("[CalculaValor_PorValor] Valor original de numero {0}", numero);
+            CalculaValor_PorValor(numero);
+            Console.WriteLine("Valor após chamar o método {0}", numero);
+        }
+
+
+        static void CalculaValor_PorReferencia(ref int valor)
+        {
+            valor = valor + 1;
+        }
+
+        public void PorReferencia()
+        {
+            int numero = 500;
+
+            Console.WriteLine("[CalculaValor_PorReferencia] Valor original de numero {0}", numero);
+            CalculaValor_PorReferencia(ref numero);
+            Console.WriteLine("Valor após chamar o método {0}", numero);
+
+        }
+
+        static void CalculaValor_PorReferencia_Out(out int valor)
+        {
+            valor = 5;
+            valor = valor + 1;
+        }
+
+        public void PorReferencia_Out()
+        {
+
+            int numero = 500;
+
+            Console.WriteLine("[CalculaValor_PorReferencia_Out] Valor original de numero {0}", numero);
+            CalculaValor_PorReferencia_Out(out numero);
+            Console.WriteLine("Valor após chamar o método {0}", numero);
+        }
+
+
+    }
+}
---
+++ Aula 14 - Metodos/ProcedimentosFuncoes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aula_14___Metodos;
+
+namespace Aula_14___Metodos
+{
+    class ProcedimentosFuncoes
+    {
+
+
+        //PROCEDIMENTO
+        static void Teste()
+        {
+            Console.WriteLine("Aqui não retorna nenhum valor");
+        }
+
+        //FUNÇÃO
+        static int Soma(int valor1, int valor2)
+        {
+            return valor1 + valor2;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("ProcedimentosFuncoesParametros");
+            PassagemDeParametrosPorValorReferenciaOUT metodo = new PassagemDeParametrosPorValorReferenciaOUT();
+
+            metodo.PorValor();
+            metodo.PorReferencia();
+            metodo.PorReferencia_Out();
+
+            Console.ReadKey();
+
+        }
+    }
+}

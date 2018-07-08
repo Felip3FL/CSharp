5d0143d3f80f66e145c3482a625ae5e113d8196e
Felipe Lima <felip3.fl@msn.com>
2018-07-07 23:00:14 -0300

    Aula 16 - Classes

---
+++ Aula 16 - Classes/Carro.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_16___Classes
+{
+
+
+    class Carro
+    {
+        private string cor;
+
+        public string Cor
+        {
+            get { return cor; }
+            set
+            {
+                if (value.Contains("Prata Preto Branco"))
+                {
+                    cor = value;
+                }
+                else
+                {
+                    throw new Exception("Cores disponíveis: Prata, Preto e Branco");
+        
+                }
+            }
+
+        }
+
+        //metodo
+        public string Descricao()
+        {
+            return "O carro é : " + Cor;
+        }
+
+        //Construtor
+
+        public Carro(String cor)
+        {
+            this.Cor = "";
+        }
+
+    }
+
+
+}
---
+++ Aula 16 - Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_16___Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Carro gol; //Definir uma variavel
+            gol = new Carro(); //Instancia da classe, cria o objeto
+
+            gol.Cor = "Prata Preto Branco";
+
+
+            Console.WriteLine(gol.Descricao());
+            Console.ReadKey();
+
+        }
+
+       
+    }
+}

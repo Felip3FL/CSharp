71b3a500ce0517b6aa35bfeb3ece9167f4c9d38a
Felipe Lima <felip3.fl@msn.com>
2018-07-08 13:58:38 -0300

    Aula 18 - Classe e Heranca

---
+++ Aula 18 - Classe e Heranca/ClasseEHeranca.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_18___Classe_e_Heranca
+{
+    class ClasseEHeranca
+    {
+        static void Main(string[] args)
+        {
+
+            PessoaFisica felipe = new PessoaFisica();
+
+            felipe.nome = "Felipe";
+            felipe.endereço = "Rua 10";
+            felipe.rg = "66.666.666-66";
+            felipe.cpf = "666.666.666-66";
+            felipe.TesteOcutarMembros();
+
+
+            //Para Acessar um membro oculto  
+            PessoaFisica pedroPessoaFisica = new PessoaFisica();
+            Pessoa pedro = (PessoaFisica)pedroPessoaFisica;
+
+            pedroPessoaFisica.TesteOcutarMembros();
+            pedro.TesteOcutarMembros();
+
+            //Aluno joao = new Aluno();
+
+            //joao.nome = "Felipe";
+            //joao.endereço = "Rua 10";
+            //joao.rg = "66.666.666-66";
+            //joao.cpf = "666.666.666-66";
+            //joao.matricula = "RA00000";
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Aula 18 - Classe e Heranca/Pessoa.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_18___Classe_e_Heranca
+{
+    class Pessoa
+    {
+        public string nome;
+        public string endereço;
+
+        public Pessoa()
+        {
+            Console.WriteLine("Criei uma pessoa");
+        }
+
+        public void TesteOcutarMembros()
+        {
+            Console.WriteLine("Teste 1");
+        }
+
+    }
+
+    class PessoaFisica : Pessoa
+    {
+        public string rg;
+        public string cpf;
+
+        public PessoaFisica()
+        {
+            Console.WriteLine("Criei uma PessoaFisica");
+        }
+
+        new public void TesteOcutarMembros()
+        {
+            Console.WriteLine("Teste 2");
+        }
+
+    }
+
+    //class Aluno : PessoaFisica
+    //{
+    //    public string matricula;
+
+    //}
+
+
+    //class PessoaJuridica : Pessoa
+    //{
+    //    public string rg;
+    //    public string cpf;
+
+    //    public PessoaJuridica()
+    //    {
+    //        Console.WriteLine("Criei uma PessoaJuridica");
+    //    }
+
+    //}
+
+
+
+
+}

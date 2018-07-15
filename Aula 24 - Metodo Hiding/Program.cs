6b380ba19066bdfea156c2681c227da15a7f3300
Felip3FL <felip3.fl@gmail.com>
2018-07-15 17:05:03 -0300

    Aula 24 - Metodo Hiding

---
+++ Aula 24 - Metodo Hiding/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_24___Metodo_Hiding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro wolf = new Cachorro();
+            wolf.Comer();
+            Console.ReadKey();
+        }
+    }
+
+
+    public class Animal
+    {
+        public virtual void Mover()
+        {
+            Console.WriteLine("Um animal se move");
+        }
+        public void Comer()
+        {
+            Console.WriteLine("Um animal come para viver");
+        }
+    }
+
+    public class Cachorro : Animal
+    {
+        public override void Mover()
+        {
+            Console.WriteLine("Um cachorro se move sobre 4 patas");
+        }
+
+        //method hiding (ocultando o método Comer da classe base)
+        public new void Comer()
+        {
+            Console.WriteLine("Um cão come ração");
+            base.Comer();
+        }
+
+    }
+
+}

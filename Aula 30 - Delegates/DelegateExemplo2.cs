bbc8c5fe09152b35ecd08ed9efe29327efb0f947
Felip3FL <felip3.fl@gmail.com>
2018-08-12 15:10:36 -0300

    Aula 30 - Delegates

---
+++ Aula 30 - Delegates/DelegateExemplo2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Aula_30___Delegates
+{
+    public class DelegateExemplo2
+    {
+        static FileStream fs;
+        static StreamWriter sw;
+
+        //declarar delegate
+        public delegate void ImprimirTexto(string texto);
+
+        //imprimir no console
+        public static void ImprimeNoConsole(string texto)
+        {
+            Console.WriteLine(texto);
+        }
+
+        //imprime em arquivo
+        public static void ImprimeEmArquivo(string texto)
+        {
+            fs = new FileStream(@"C:\Users\fl\Desktop\audio.log", FileMode.Append, FileAccess.Write);
+            sw = new StreamWriter(fs);
+            sw.WriteLine(texto);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+
+        public static void Teste()
+        {
+            //criar instancias do delegate
+            ImprimirTexto it1 = ImprimeNoConsole;
+            ImprimirTexto it2 = ImprimeEmArquivo;
+
+            Console.WriteLine("Imprime no console");
+            //invocar delegate
+            it1("Felip3FL - no console");
+            Console.WriteLine("Imprime no arquivo");
+            it2("Felip3FL - no arquivo");
+        }
+
+    }
+}
---
+++ Aula 30 - Delegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_30___Delegates
+{
+    class Program
+    {
+
+        //declaracao do delegate com definição dos parametros e tipo de retorno
+        delegate void SimplesDelegate(string texto);
+
+        static void Main(string[] args)
+        {
+            //criar instancia do delegate
+            //SimplesDelegate sd = new SimplesDelegate(ImprimeTexto);
+            SimplesDelegate sd1 = ImprimeTexto;
+            sd1("FELIPE " + DateTime.Today.ToShortDateString());
+
+            DelegateExemplo2.Teste();
+
+            Console.ReadKey();
+
+        }
+
+        //definição do método para usar com o delegate
+        //ele deve possuir a mesma assinatura do delegate
+        static private void ImprimeTexto(string texto)
+        {
+            Console.WriteLine(texto);
+        }
+    }
+}

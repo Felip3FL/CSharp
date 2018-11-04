52a64cd36b889b76aa3a004e4eaefd38bd7966ae
Felip3FL <felip3.fl@gmail.com>
2018-11-04 16:46:04 -0200

    Aula 36 - LINQ Operadores de Consulta

---
+++ Aula 36 - LINQ Operadores de Consulta/IListExtensao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_36___LINQ_Operadores_de_Consulta
+{
+    public static class IListExtensao
+    {
+        //Neste exemplo tomamos uma instância da classe que implementa a interface de
+        //uma lista de inteiros IList<int> e aumentamos os seus valores com uma quantidade
+        //O método de extensão AumentarValorLista pode acessar somente os elementos incluidos na interface IList
+        public static void AumentarValorLista(this IList<int> lista, int quantidade)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                lista[i] += quantidade;
+            }
+        }
+    }
+}
---
+++ Aula 36 - LINQ Operadores de Consulta/MetodoExtensao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_36___LINQ_Operadores_de_Consulta
+{
+    public static class MetodoExtensao
+    {
+        //estende a classe string 
+        //indicado pela palavra this antes do nome do primeiro argumento do método
+        public static int ContaCaracteres(this string texto)
+        {
+            var Contador = texto.Length;
+            return Contador;
+        }
+
+        //estende a classe string
+        public static int ContaPalavras(this string texto)
+        {
+            return texto.Split(new char[] { ' ', ',', '?', '!' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+    }
+}

6d7153dc84d27bffa7258ed06ed7af4ef6267349
Felip3FL <felip3.fl@gmail.com>
2018-11-22 02:00:22 -0200

    Aula 39 - LINQ to XML

---
+++ Aula 39 - LINQ to XML/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Aula_39___LINQ_to_XML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnXDocument = new System.Windows.Forms.Button();
+            this.btnXElement = new System.Windows.Forms.Button();
+            this.txtXmlDoc = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(551, 461);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(125, 48);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
+            // btnXDocument
+            // 
+            this.btnXDocument.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnXDocument.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXDocument.Location = new System.Drawing.Point(15, 461);
+            this.btnXDocument.Margin = new System.Windows.Forms.Padding(4);
+            this.btnXDocument.Name = "btnXDocument";
+            this.btnXDocument.Size = new System.Drawing.Size(199, 48);
+            this.btnXDocument.TabIndex = 4;
+            this.btnXDocument.Text = "Criar XML : XDocument";
+            this.btnXDocument.UseVisualStyleBackColor = false;
+            this.btnXDocument.Click += new System.EventHandler(this.btnXDocument_Click);
+            // 
+            // btnXElement
+            // 
+            this.btnXElement.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnXElement.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXElement.Location = new System.Drawing.Point(295, 461);
+            this.btnXElement.Margin = new System.Windows.Forms.Padding(4);
+            this.btnXElement.Name = "btnXElement";
+            this.btnXElement.Size = new System.Drawing.Size(195, 48);
+            this.btnXElement.TabIndex = 5;
+            this.btnXElement.Text = "Criar XML : XElement";
+            this.btnXElement.UseVisualStyleBackColor = false;
+            this.btnXElement.Click += new System.EventHandler(this.btnXElement_Click);
+            // 
+            // txtXmlDoc
+            // 
+            this.txtXmlDoc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.txtXmlDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXmlDoc.Location = new System.Drawing.Point(13, 13);
+            this.txtXmlDoc.Margin = new System.Windows.Forms.Padding(4);
+            this.txtXmlDoc.Multiline = true;
+            this.txtXmlDoc.Name = "txtXmlDoc";
+            this.txtXmlDoc.Size = new System.Drawing.Size(666, 443);
+            this.txtXmlDoc.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 520);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnXDocument);
+            this.Controls.Add(this.btnXElement);
+            this.Controls.Add(this.txtXmlDoc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnXDocument;
+        private System.Windows.Forms.Button btnXElement;
+        private System.Windows.Forms.TextBox txtXmlDoc;
+    }
+}
+
---
+++ Aula 39 - LINQ to XML/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Aula_39___LINQ_to_XML
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXDocument_Click(object sender, EventArgs e)
+        {
+            txtXmlDoc.Text = CriarDocumentoXMLviaXDocument().ToString();
+        }
+
+        private XDocument CriarDocumentoXMLviaXDocument()
+        {
+
+            //Criar XML
+            XDocument clientesXML = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                                new XComment("Criando XML via LINQ to XML - XDocument"),
+                                new XElement("clientes",
+                                    new XElement("cliente",
+                                        new XAttribute("codigo", 1),
+                                        new XElement("nome", "Felipe"),
+                                        new XElement("email", "felip3.fl@gmail.com")),
+                                    new XElement("cliente",
+                                        new XAttribute("codigo", 2),
+                                        new XElement("nome", "Pamella"),
+                                        new XElement("email", "pamella@gmail.com"))));
+
+            clientesXML.Save(@"C:\Users\fl\Desktop\MeuXML.xml");
+
+            return clientesXML;
+        }
+
+
+        private XElement CriarDocumentoXMLViaXElement()
+        {
+            XElement xmlDoc =
+             new XElement("clientes",
+             new XElement("cliente",
+                new XAttribute("codigo", "1"),
+                    new XElement("nome", "Felipe"),
+                    new XElement("email", "felip3.fl@gmail.com")),
+             new XElement("cliente",
+                 new XAttribute("codigo", "2"),
+                    new XElement("nome", "Pamella"),
+                    new XElement("email", "pamella@bol.com.br")));
+
+            xmlDoc.Save(@"C:\Users\fl\Desktop\MeuXML.xml");
+
+            return xmlDoc;
+        }
+
+        private void btnXElement_Click(object sender, EventArgs e)
+        {
+            txtXmlDoc.Text = CriarDocumentoXMLViaXElement().ToString();
+        }
+    }
+}
